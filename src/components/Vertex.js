e92e370d218afd46fb6b7fe07760d457af5f0b73
Nicolai Dørfler <ndoe@itu.dk>
2019-10-24 12:58:26 +0200

    Vertex component added

---
+++ src/components/Vertex.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+
+function Vertex (props) { 
+   let vertices= [];   
+};
+
+function addVertex(v){
+
+}
+
+const filename = "test.git";
+const dependendcies = ["test","egwgew",]
+
+export default Vertex;
+
+
+
