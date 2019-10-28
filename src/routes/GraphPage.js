ff27bd54471b4d49e7ad3b4c5930c41cdbb24582
Doeffe <ndoe@itu.dk>
2019-10-28 10:27:03 +0100

    Moved graph logic to graphpage

--- src/routes/GraphPage.js
+++ src/routes/GraphPage.js
@@ -1,8 +1,54 @@
 import React from 'react';
-import Vertex from '../components/Vertex';
+import { ForceGraph2D, ForceGraph3D, ForceGraphVR } from 'react-force-graph';
 
 export default class GraphPage extends React.Component { 
+  genRandomTree(N = 300, reverse = false) {
+    return {
+      nodes: [...Array(N).keys()].map(i => ({ id: i })),
+        links: [...Array(N).keys()]
+      .filter(id => id)
+      .map(id => ({
+        [reverse ? 'target' : 'source']: id,
+        [reverse ? 'source' : 'target']: Math.round(Math.random() * (id-1))
+      }))
+    };
+  }
+
   render() {
-    return <h1> </h1>
+
+    const myData = {
+      "nodes": [ 
+          { 
+            "id": "id1",
+            "name": "App.js",
+            "val": 10
+          },
+          { 
+            "id": "id2",
+            "name": "Component2.js",
+            "val": 10 
+          }
+      ],
+      "links": [
+          {
+              "source": "id1",
+              "target": "id2"
+          }
+      ]
+    }
+
+    const GROUPS = 12;
+    const gData = this.genRandomTree();
+
+    return (
+      <div>
+        <ForceGraph3D
+          graphData={gData}
+          nodeAutoColorBy={d => d.id%GROUPS}
+          linkAutoColorBy={d => gData.nodes[d.source].id%GROUPS}
+          linkWidth={2}
+        />
+      </div>
+    )
   }
 }
