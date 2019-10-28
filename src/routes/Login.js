25d7a52e10f4545e557a20c69b2c7b129b056de7
Doeffe <ndoe@itu.dk>
2019-10-28 15:12:37 +0100

    Merged upload and vertex
    merging upload branch and vertex to use @Nikolaj's drag and drop feature.

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,23 +1,11 @@
 import React from 'react'
-import { ForceGraph2D, ForceGraph3D, ForceGraphVR } from 'react-force-graph';
+
 
 export default class Login extends React.Component {
   render() {   
 
     return (
-<<<<<<< HEAD
       <div> Welcome   </div>
-=======
-      <div>
-        <ForceGraph3D
-          backgroundColor={"#eeeeee"}
-          graphData={gData}
-          nodeAutoColorBy={d => d.id%GROUPS}
-          linkAutoColorBy={d => gData.nodes[d.source].id%GROUPS}
-          linkWidth={2}
-        />
-      </div>
->>>>>>> upload-and-read-files
     )
   }
 }
