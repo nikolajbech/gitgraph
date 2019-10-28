1182e98bf6756c178a8c6db97c83d856ecc522dd
Athystrup <andreas@thystrup.dk>
2019-10-28 10:12:35 +0100

    Login with GitHub through firebase

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,13 +5,49 @@ import '../styles/Home.css'
 import firebase from 'firebase';
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 
+firebase.initializeApp({
+  apiKey: "AIzaSyCTEYl4ZF6SXsQ3rqu1OUTQnlcYRsGBG8M",
+  authDomain: "gitgraphpro.firebaseapp.com"
+})
+
 export default class Home extends React.Component {
+  state={isSignedIn: false}
+  uiConfig = {
+    signInFlow: "popup", 
+    signInOptions: [
+      firebase.auth.GithubAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      signInSuccess: () => false
+    }
+  }
+
+  componentDidMount = () => {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ isSignedIn: !!user})
+      console.log("user",user)
+    })
+  }
+
   render() {
     return(
       <div>
-        <h1>Home</h1>
-        <Button intent="success" text="button content" />
-
+      {this.state.isSignedIn ? (
+        <span>
+          <div>Signed In!</div>
+          <button Intent="success" onClick={() => firebase.auth().signOut()}>Sign out!</button>
+          <h1>Welcome {firebase.auth().currentUser.displayName}</h1>
+          <img
+            alt="profile picture"
+            src={firebase.auth().currentUser.photoURL}
+          />
+        </span>
+      ) : (
+        <StyledFirebaseAuth
+          uiConfig={this.uiConfig}
+          firebaseAuth={firebase.auth()}
+        />
+      )}
       </div>
     )
   }
