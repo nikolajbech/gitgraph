4cf85aed19034c7c4f67e938653008d32d95f001
Nikolaj Bech Andersen <nikolajbechandersen@Nikolajs-MacBook-Pro.local>
2019-11-14 13:06:36 +0100

    Nice ui

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import '../styles/Home.css';
 
-export default class Home extends React.Component {
+const logo = 'https://firebasestorage.googleapis.com/v0/b/gitgraphpro.appspot.com/o/logo.png?alt=media&token=8d0d85d5-8d86-4c38-834f-f1b8d61d347d'
 
+export default class Home extends React.Component {
 
   render() {
     return (
       <div className={"bg"}>
         <div className={"signin"}>
-          <p>HEJFAOIEFJOAIEJ</p>
+          <img
+          src={logo}
+          style={{width: 300, height: 80, marginTop: 30}}/>
+          <input type="text" name="name" />
+          <input type="text" name="token" />
+        <div className={"fetch"}>
+
+        </div>
         </div>
       </div>
     )
