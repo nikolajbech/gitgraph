cc9436ccda8d618e1a9cfe7ee59e887e4f917509
Nikolaj Bech Andersen <nikolajbechandersen@Nikolajs-MacBook-Pro.local>
2019-11-14 13:31:35 +0100

    Landing page UI done

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../styles/Home.css';
 
-const logo = 'https://firebasestorage.googleapis.com/v0/b/gitgraphpro.appspot.com/o/logo.png?alt=media&token=8d0d85d5-8d86-4c38-834f-f1b8d61d347d'
+const logo = 'https://firebasestorage.googleapis.com/v0/b/gitgraphpro.appspot.com/o/logoHigh.png?alt=media&token=e9a1bcc9-819e-43c3-9435-b3c9770c912d'
 
 export default class Home extends React.Component {
 
@@ -11,12 +11,14 @@ export default class Home extends React.Component {
         <div className={"signin"}>
           <img
           src={logo}
-          style={{width: 300, height: 80, marginTop: 30}}/>
-          <input type="text" name="name" />
-          <input type="text" name="token" />
-        <div className={"fetch"}>
-
-        </div>
+          style={{width: 300, height: 80, marginTop: 30, marginBottom: 20}}/>
+          <input type="text" name="name" placeholder="Username" style={{marginBottom: 20, borderWidth: 0, borderBottomWidth: 1, borderColor: '#707070', outline: 'none', fontSize: 17, width: '100%', padding: 20 }} />
+          <input type="text" name="token" placeholder="GitHub Token" style={{marginBottom: 20, borderWidth: 0, borderBottomWidth: 1, borderColor: '#707070', outline: 'none', fontSize: 17, width: '100%', padding: 20 }} />
+        <a style={{textDecoration: 'none'}} onClick={() => console.log("clicked")}>
+          <div className={"fetch"}>
+            <p style={{color: 'white', fontSize: 24}}>Fetch Repos</p>
+          </div>
+        </a>
         </div>
       </div>
     )
